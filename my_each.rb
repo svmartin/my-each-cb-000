fbada40a176050d005a249c45a931521f86e3751
Sean Martin <sean.martin@gmail.com>
2018-09-20 03:01:10 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each(array) # put argument(s) here
   # code here
   count = 0
   while count < array.size
-    yield array[i]
-    i += 1
+    yield array[count]
+    count += 1
   end
   array
 end
